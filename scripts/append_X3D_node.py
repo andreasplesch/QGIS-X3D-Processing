57bb91564c0896b80d6bff52bf8a17f8db023689
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-03-17 13:40:12 -0400

    Add files via upload

--- scripts/append_X3D_node.py
+++ scripts/append_X3D_node.py
@@ -8,11 +8,18 @@ append x3d node to existing xml x3d scene
 ##output_X3D_scene=output file
 
 import xml.etree.ElementTree as ET
+#import xml.dom.minidom as DOM
 
 doc = ET.parse(X3D_scene)
+#doc=DOM.parse(X3D_scene)
 
 x3d = doc.getroot() # should be X3D element
 scene = x3d.find('Scene')
+#scene = doc.getElementsByTagName('Scene')[0]
 scene.append(ET.parse(X3D_node).getroot())
+#x3dNodeElement = DOM.parse(X3D_node).documentElement
+#scene.appendChild(x3dNodeElement)
 
-doc.write(output_X3D_scene, 'UTF-8', True)
+doc.write(output_X3D_scene, 'utf-8', True)
+#f=open(output_X3D_scene, 'w')
+#doc.writexml(f, '', '  ', '', encoding='utf-8') # turns out to be not so pretty
