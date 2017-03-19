0bc9aae964e96e45354505c5b0cdb3f5ced83573
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-03-19 16:52:45 -0400

    Add files via upload

---
+++ scripts/X3D_color_string_from_colorize_color.py
@@ -0,0 +1,19 @@
+'''
+QGIS Processing script
+(c) 2017 Andreas Plesch
+extracts and returns colorize color as string from raster layer
+or custom color if requested
+'''
+##X3D=group
+##color_string_from_colorize_color=name
+##input_raster=raster
+##extract_colorize_color=boolean True
+##custom_color=string 0.8 0.8 0.7
+##output_color_string=output string
+
+r = processing.getObject(input_raster)
+output_color_string = custom_color  # needs some sanity checking
+if extract_colorize_color:
+    colorize = r.hueSaturationFilter().colorizeColor()
+    output_color_string = '%s %s %s' % (colorize.redF(), colorize.greenF(), colorize.blueF())
+print(output_color_string)
