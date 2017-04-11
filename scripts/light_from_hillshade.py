dd346875f7386b3c4964741b173e7fffce0854f6
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-04-11 10:06:48 -0400

    single light

--- scripts/light_from_hillshade.py
+++ scripts/light_from_hillshade.py
@@ -4,28 +4,16 @@ QGIS Processing script
 constructs directional light from azimuth and altitude
 '''
 ##X3D=group
-##light from hiillshade=name
+##light from hillshade=name
 ##azimuth=number 315
 ##altitude=number 45
 ##multidirectional=boolean False
 ##output_X3D_light=output string
 
-import PyQt4.QtXml as qxml
+out ='<Transform rotation="0 1 0 %s">\n’ % azimuth
+out+='    <Transform rotation="1 0 0 %s">\n’ % altitude
+out+='        <DirectionalLight />\n'
+out+='    </Transform>\n'
+out+='</Transform>\n'
 
-dem = processing.getObject(input_raster)
-r=dem.renderer()
-
-out_azimuth=custom_azimuth
-out_altitude=custom_altitude
-out_zFactor=custom_zFactor
-out_multidirectional=float(custom_multidirectional)
-
-if r.type() == u'hillshade' and not(use_custom_parameters):
-    doc = qxml.QDomDocument()
-    lElement=doc.createElement('rasterlayer') #some required parent tag
-    r.writeXML(doc, lElement)
-    rElement=lElement.firstChild().toElement()
-    out_azimuth = float(rElement.attribute('azimuth'))
-    out_altitude = float(rElement.attribute('angle'))
-    out_zFactor = float(rElement.attribute('zfactor'))
-    out_multidirectional= float(rElement.attribute('multidirection'))
+output_X3D_light=out
