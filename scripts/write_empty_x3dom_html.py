b54b91c9c2c496ae6707b76074366086eba80694
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-04-30 16:15:23 -0400

    use sun position, toogle controls
    sun position at any given time for DEM location with time slider, toggle controls with 'z'

--- scripts/write_empty_x3dom_html.py
+++ scripts/write_empty_x3dom_html.py
@@ -9,16 +9,15 @@ write out a full screen style html x3dom template file
 
 f=open(output_file,'w')
 f.write('''<!DOCTYPE html>
-<html style='width:100%; height:100%; border:0; margin:0; padding:0;'>
+<html style="width:100%; height:100%; border:0; margin:0; padding:0;">
   
   <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>    
-    <meta http-equiv='Content-Type' content='text/html;charset=utf-8'></meta>
-    <script type='text/javascript' src='https://www.x3dom.org/release/x3dom-full.js'> </script>
-    <script type="text/javascript" src=
-    'https://cdn.rawgit.com/andreasplesch/e2cd02fd30ff74670e00758e46c1679c/raw/08cad8f391fe6242fe7e44373df0d9dead00d8b6/x3dom_HDpick.user.js'
-    > </script>
-    <link rel='stylesheet' type='text/css' href='https://www.x3dom.org/release/x3dom.css'/>
+    <meta content="IE=edge" http-equiv="X-UA-Compatible" />    
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type" />
+    <script src="https://www.x3dom.org/release/x3dom-full.js" type="text/javascript"></script>
+    <script src="https://cdn.rawgit.com/andreasplesch/e2cd02fd30ff74670e00758e46c1679c/raw/08cad8f391fe6242fe7e44373df0d9dead00d8b6/x3dom_HDpick.user.js" type="text/javascript"></script>
+    <script src="https://cdn.rawgit.com/mourner/suncalc/master/suncalc.js" type="text/javascript"></script>
+    <link href="https://www.x3dom.org/release/x3dom.css" rel="stylesheet" type="text/css"/>
     <style>
         div#hud {
             position:  fixed;
@@ -54,31 +53,71 @@ f.write('''<!DOCTYPE html>
         }
         #lightdot:hover {
             fill: red;
-            cursor: move;
+          cursor: move;
+            
         }
         #lightcircle:hover {
             cursor: crosshair;
         }
+        
         .lighttext::selection {
             background: none;
         }
         .lighttext {
             font-size: 14px;
         }
+        #timecontrol {
+            position: fixed;
+            margin-left: 30%;
+            margin-right: 30%;
+            width: 40%;
+            padding: 10px;
+            z-index: 9999;
+            background: rgba(255, 255, 255, 0.5);
+        }
+        #timelabel {
+            display: block;
+            text-align: center;
+            font-size: 14px;
+        }
+        #timeslider {
+            width: 100%;
+        }
+        .timeZoomButton {
+          font-size: 20px;
+        }
+        .timeZoomButton:hover {
+          cursor: crosshair;
+          background: red;
+        }
+        #timeMax {
+          float: right;
+        }
     </style>
   </head>
 
-<body style='width:100%; height:100%; border:0; margin:0; padding:0; background: linear-gradient(Grey 0%, White 100%);'>
-    <div id='hud'> HUD area </div>
-    <div id="lightcontrol">
-        <svg id="lightsvg" baseprofile="full" version="1.1" >
-            <circle id="lightcircle" cx="50%" cy="50%" fill="white" opacity="0.5" r="100" stroke="black" stroke-width="2"></circle>
-            <circle cx="50%" cy="50%" fill="transparent" r="2" stroke="black" stroke-width="2"></circle>
-            <text id="azimuthtext"  class="lighttext" text-anchor="start" x="0" y="12">0°</text>
-            <text id="altitudetext"  class="lighttext" text-anchor="end" x="100%" y="12">90°</text>
-            <circle cx="50%" cy="50%" fill="black" id="lightdot" r="10" ></circle>
-        </svg>
-    </div>
+<body style="width:100%; height:100%; border:0; margin:0; padding:0; background: linear-gradient(Grey 0%, White 100%);">
+  <div id="hud"> HUD area </div>
+  <div title="control lighting by dragging dot, toggle with 'z'" id="lightcontrol">
+      <svg id="lightsvg" baseprofile="full" version="1.1" >
+          <circle id="lightcircle" cx="50%" cy="50%" r="100" fill="none" stroke="black" stroke-width="2"
+                  pointer-events="visible" ></circle>
+          <line x1="50%" x2="50%" y1="5%" y2="15%" stroke="black" stroke-width="2"/> 
+          <circle cx="50%" cy="50%" fill="transparent" r="2" stroke="black" stroke-width="2"></circle>
+          <text id="azimuthtext"  class="lighttext" text-anchor="start" x="0" y="12">000°</text>
+          <text id="altitudetext"  class="lighttext" text-anchor="end" x="100%" y="12">90°</text>
+          <circle cx="50%" cy="50%" fill="black" id="lightdot" r="10"></circle>
+      </svg>
+  </div>
+  <div title="toggle with 'z'" id="timecontrol">
+    <button title="zoom into time period" id="timeZoomIn" class="timeZoomButton"> + </button>
+    <button title="recenter time period" id="timeRecenter" class="timeZoomButton"> = </button>
+    <button title="zoom out of time period" id="timeZoomOut" class="timeZoomButton"> - </button>
+    <label id="timelabel" for="timeslider">pick a time </label>
+    <input id="timeslider" type="range" min="0" max="1000000" step="1"></input>
+    <span id="timeMin">0</span>
+    <span id="timeMax">0</span>
+  </div>
     <X3D id='x3dElement' showStat='false' showLog='false'>
         <Scene DEF='scene'>
             <NavigationInfo headlight='false'></NavigationInfo>
@@ -87,6 +126,125 @@ f.write('''<!DOCTYPE html>
     <script type="text/javascript">
         //<![CDATA[
         document.onload = function () {
+            
+            var degGlyph = "°";
+            var x3dElement = document.querySelector('X3D');
+            var scene = x3dElement.querySelector('scene');
+            
+            var timeControl = document.querySelector('#timecontrol');
+            var lightControl = document.querySelector('#lightcontrol');
+            
+            var controlsDisplay = 3;
+            // toggle controls
+            x3dElement.addEventListener('keypress', toggleControls, false);
+          
+            function toggleControls (e) {
+              if (e.charCode !== 122) {return;} //'z'
+              
+              var timeControlDisplay = 'none';
+              var lightControlDisplay = 'none';
+              
+              controlsDisplay = ++controlsDisplay % 4 ;
+              if (controlsDisplay & 1) {
+                lightControlDisplay = 'block';
+              }
+              if (controlsDisplay & 2) {
+                timeControlDisplay = 'block';
+              }
+              
+              timeControl.style.display = timeControlDisplay;
+              lightControl.style.display = lightControlDisplay;
+            }
+          
+              
+            // time control
+            var timeLabel = document.querySelector('#timelabel');
+            var timeSlider = document.querySelector('#timeslider');
+            var timeMin = document.querySelector('#timeMin');
+            var timeMax = document.querySelector('#timeMax');
+            var timeZoomIn = document.querySelector('#timeZoomIn');
+            var timeZoomOut = document.querySelector('#timeZoomOut');
+            var timeRecenter = document.querySelector('#timeRecenter');
+            var time = new Date();
+            var timeCenter = Date.now();
+            var timeRange = 7 * 24 * 3600 * 1000;
+            
+            function updateTimeRange () {
+              timeSlider.min = timeCenter - timeRange/2;
+              timeSlider.max = timeCenter + timeRange/2;
+              timeMin.textContent = new Date(1.0 * timeSlider.min).toDateString();
+              timeMax.textContent = new Date(1.0 * timeSlider.max).toDateString();
+            };
+          
+            updateTimeRange ();
+            timeSlider.value = timeCenter;
+            
+            timeZoomIn.addEventListener('click', zoomInTimeRange, false); 
+            timeZoomOut.addEventListener('click', zoomOutTimeRange, false); 
+            timeRecenter.addEventListener('click', recenterTimeRange, false); 
+            
+            function recenterTimeRange (e) {
+              timeCenter = timeSlider.valueAsNumber;
+              updateTimeRange();
+            }
+            function zoomInTimeRange (e) {
+              timeRange /= 2;
+              updateTimeRange();
+            }
+            function zoomOutTimeRange (e) {
+              timeRange *= 2;
+              updateTimeRange();
+            }    
+        
+            var geoCenter = document.querySelector('GeoViewpoint').getAttribute("centerOfRotation").split(" ");
+            
+            var r2d = 180/Math.PI;
+          
+            timeSlider.addEventListener('input', updateTime, false);
+            timeSlider.addEventListener('change', updateTime, false); //for IE
+            
+            function updateTime (e) {
+              time.setTime(e.target.valueAsNumber);
+              timeLabel.textContent = time.toDateString();
+              timeLabel.textContent += ", " + padZeroes(time.getHours(),2) + ":" + padZeroes(time.getMinutes(), 2) ;
+              timeLabel.textContent += " (UTC-" + time.getTimezoneOffset()/60 + "h)" ;
+              
+              var sunPosition = SunCalc.getPosition(time, 1.0 * geoCenter[0], 1.0 * geoCenter[1]);
+              var al = sunPosition.altitude * r2d; 
+              var az = (sunPosition.azimuth * r2d + 180 ) % 360;
+              
+              updateLightDot(az, al);
+              
+              updateDirLight(az, al);
+            }
+          
+            function updateLightDot(az, al) {
+              var r = (90 - al) / 180; //0.5 * (90 - al) / 90 ;
+              var a = (az - 90) / r2d;
+              var x = r * Math.cos(a) * lightRect.width;
+              var y = r * Math.sin(a) * lightRect.height;
+              
+              var cx = lightCenterSVG.x + x ;
+              var cy = lightCenterSVG.y + y ;
+              lightDot.setAttribute('cx', cx);
+              lightDot.setAttribute('cy', cy);
+            }
+          
+            function updateDirLight(az, al) {
+              azimuthText.textContent = padZeroes(az, 3) + degGlyph;
+              altitudeText.textContent = padZeroes(al, 2) + degGlyph;
+                
+              if (dirLight) {
+                var alRot = dirLight.parentNode;
+                alRot.setAttribute('rotation', '1 0 0 ' + (-al / r2d));
+                var azRot = alRot.parentNode;
+                azRot.setAttribute('rotation', '0 1 0 ' + (180 - az) / r2d);
+              }
+            }
+          
+            function padZeroes (number, digits) {
+              return ("00000000" + number.toFixed(0)).slice(-digits);
+            }
           
             // light control
             var lightCircle = document.querySelector('#lightcircle');
@@ -97,8 +255,17 @@ f.write('''<!DOCTYPE html>
             var altitudeText = document.querySelector('#altitudetext');
             var lightDot =  document.querySelector('#lightdot');
             
-            var r2d = 180/Math.PI;
-          
+            // init control
+            var dirLight = scene.querySelector('DirectionalLight');
+            if (dirLight) {
+              var alRot = dirLight.parentNode;
+              var al = -alRot.getAttribute('rotation').split(' ')[3] * r2d;
+              var azRot = alRot.parentNode;
+              var az = (180 - azRot.getAttribute('rotation').split(' ')[3] * r2d + 360) % 360;
+              updateDirLight(az, al);
+              updateLightDot(az, al);
+            }
+            
             lightCircle.addEventListener('touchmove', updateLight, false);
             lightDot.addEventListener('touchmove', updateLight, false);
             lightCircle.addEventListener('mousemove', updateLight, false);
@@ -122,28 +289,20 @@ f.write('''<!DOCTYPE html>
                 x /= lightRect.width ;
                 y /= lightRect.height ;
                 var al = 90 - Math.sqrt(x*x + y*y) * 180; // no Math.hypot in IE
-                azimuthText.textContent=("00"+az.toFixed(0)).slice(-3)+"°";
-                altitudeText.textContent=("0"+al.toFixed(0)).slice(-2)+"°";
-                var dirLight = scene.querySelector('DirectionalLight');
-                if (dirLight) {
-                    var alRot = dirLight.parentNode;
-                    alRot.setAttribute('rotation', '1 0 0 ' + (-al / r2d));
-                    var azRot = alRot.parentNode;
-                    azRot.setAttribute('rotation', '0 1 0 ' + (180 - az) / r2d);
-                }
+                updateDirLight(az, al);
             }
             // picking, lights
-            var x3dElement = document.querySelector('X3D');
-            var scene = x3dElement.querySelector('scene');
             var hud = document.querySelector('#hud');
             
-            // switch headlight on if no other lights
+          // switch headlight on if no other lights
             var lights = scene.querySelector('PointLight, DirectionalLight, SpotLight');
             var runtime = x3dElement.runtime ;
             var navInfo = runtime.getActiveBindable('NavigationInfo') ;
             if (!lights) {
                 navInfo.setAttribute('headlight', true);
             }
+            
+            scene.addEventListener('mousemove', updatePointing, false);//improved but slow picking with HD picker addon
             
             x3dElement.addEventListener('keypress', toggleHeadlight, false);
             
@@ -152,16 +311,14 @@ f.write('''<!DOCTYPE html>
                 var hl = navInfo.getFieldValue('headlight');
                 navInfo.setFieldValue('headlight', !hl);
             }
-            
-            scene.addEventListener('mousemove', updatePointing, false);//improved but slow picking with HD picker addon
-            
+          
             function updatePointing (event) {
                 //update HUD
                 var gc = x3dom.fields.MFVec3f.parse(event.hitPnt.toString());
                 var gd = x3dom.nodeTypes.GeoCoordinate.prototype.GCtoGD(['GC','WE'], gc)[0];
                 var veNode = event.hitObject.querySelector('[yscale]'); // any child object with yscale attribute
                 var ve = veNode ? 1.0 * veNode.yscale : 1.0 ;
-                hud.textContent = "long.: "+gd.x.toFixed(3)+"° lat.: "+gd.y.toFixed(3)+"° height: "+(gd.z/ve).toFixed(1)+"m";
+                hud.textContent = "long.: "+gd.x.toFixed(3)+degGlyph+" lat.: "+gd.y.toFixed(3)+degGlyph+" height: "+(gd.z/ve).toFixed(1)+"m";
             }
         }
         //]]>
