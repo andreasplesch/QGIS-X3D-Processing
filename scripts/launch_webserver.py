7bd32c0e4ce450276cfe2bd74d4db8fd89f380f0
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-05-20 17:09:12 -0400

    python webserver script

---
+++ scripts/launch_webserver.py
@@ -0,0 +1,40 @@
+'''
+QGIS Processing script
+(c) 2017 Andreas Plesch
+launch webserver in background with given folder as root
+'''
+##X3D=group
+##launch_webserver=name
+##root_folder=folder
+##port=number 8000
+##pid=output number
+'''
+import os
+import SimpleHTTPServer
+import SocketServer
+import threading
+'''
+from PyQt4.QtCore import QProcess
+
+p = QProcess()
+p.setWorkingDirectory(root_folder)
+args = ('-m', 'SimpleHTTPServer', str(port))
+#program = 'python -m SimpleHTTPServer %s' % (port)
+#program='notepad'
+state, pid= p.startDetached('python', args, root_folder) 
+print ("webserver process started at pid %s, status %s" % (pid, state))
+if state: progress.setInfo("webserver process started serving from port:", port)
+'''
+def webserver (root, PORT):
+
+    os.chdir(root)
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+    print ("serving at port", PORT)
+    httpd.serve_forever()
+
+t=threading.Thread(target=webserver, args=(root_folder, port))
+t.daemon=False
+t.start() # crashes
+if t.isAlive(): progress.setInfo("webserver thread started ar port:", port)
+'''
