5162da4887a0fa0ee5ae78da2db3d47b393aba3f
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-06-05 07:21:46 -0400

    Add files via upload

---
+++ scripts/serve_and_open_html.py
@@ -0,0 +1,28 @@
+'''
+QGIS Processing script
+(c) 2017 Andreas Plesch
+serve html file and show in browser
+'''
+##X3D=group
+##serve_and_open_html=name
+##input_html=file
+##server_port=number 8000
+##open_in_browser=boolean False
+##html_file_name=output string
+
+import shutil
+import webbrowser
+from PyQt4.QtCore import QUrl, QFileInfo
+
+port = int(server_port)
+
+if open_in_browser:
+    html = QFileInfo(input_html)
+    root_folder = html.canonicalPath()
+    # kills running web server if necessary
+    processing.runalg('script:launchwebserver', root_folder, port)
+    #url = QUrl.fromLocalFile(output_file).toString()
+    url = 'http://localhost:%s/%s' % (port, html.fileName())
+    webbrowser.open_new(url)
+    
+html_file_name = html.fileName()
